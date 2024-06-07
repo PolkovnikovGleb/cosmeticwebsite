c95ba07e0c0602f90dd79d680e3ffe9b28032822
PolkovnikovGleb <glebpolkwork@gmail.com>
2024-06-07 15:07:44 +0700

    prodchect init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App(): React.JSX.Element {
+  return (
+    <>
+      <div>
+        <p>Проект запущен</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
